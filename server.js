c6d02661f31058d281e9771d79feac4791b5ed06
ax4 <yaoyue718>
2016-05-21 16:39:56 -0700

    add cron to server.js, a daily clean-up of ./tmp/input & ./tmp/output
    as described

--- server.js
+++ server.js
@@ -8,6 +8,26 @@ var PythonShell = require('python-shell');
 // web content
 var client = fs.readFileSync('client.html'); 
 
+// ref: http://stackoverflow.com/questions/18052762/remove-directory-which-is-not-empty
+var deleteFolderRecursive = function (path) {
+    if (!(path == './tmp/del' || path == './tmp/input' || path == './tmp/output')) {
+        return console.log('Bad Try with rm -r'); 
+    }
+
+    if (fs.existsSync(path)) {
+        fs.readdirSync(path).forEach(function (file, index) {
+            var curPath = path + "/" + file;
+            if (fs.lstatSync(curPath).isDirectory()) { // recurse
+                deleteFolderRecursive(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(path);
+    }
+};
+
+
 // http server: send the web content
 var server = http.createServer(function (req, res) {
     res.writeHead(200, {});
@@ -70,6 +90,45 @@ ioserver.on('connection', function (socket) {
     });
 });
 
-// run the server 
+// create the cron job to clean folder
+var CronJob = require('cron').CronJob;
+//new CronJob('00 30 12 * * *', function () {
+//    console.log('You will see this message every day on 12:30 AM as LAX time.');
+//}, null, true, 'America/Los_Angeles');
+new CronJob('00 30 12 * * *', function () {
+    
+    var clean = function (path) {
+        if (!(path == './tmp/del' || path == './tmp/input' || path == './tmp/output')) {
+            return console.log('Bad Try with clean function()');
+        }
+        fs.stat(path, function (err, stats) {
+            if (err) {
+                return console.log(err);
+            }
+            if (stats.isDirectory()) {
+                deleteFolderRecursive(path);
+                console.log("rm -r " + path);
+                fs.mkdir(path, function () {
+                    console.log("Re-mkdir" + path);
+                });
+            }
+        });
+    };
+    server.close()
+    //fixme: close socket.io server
+    console.log('Server restart');
+    var my_clean_list = function () {
+        var folder_1 = './tmp/input';
+        clean(folder_1);
+        var folder_2 = './tmp/output';
+        clean(folder_2);
+    };
 
+    my_clean_list(); 
+    
+    return server.listen(port); 
+    //fixme: re-open socket.io server
+}, null, true, 'America/Los_Angeles');
+
+// run the server 
 server.listen(port); 
